a89e374085752550f366fda4f025e17175d7a47d
LuisAlvesSilva <contato.alvesluis@gmail.com>
2024-08-02 10:55:32 -0300

    modified:   script.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function studentMsg(totalScores, studentScore) {
-    avarage = getAverage(totalScores);
-    grade = getGrade(studentScore);
-    
-    return 'Class average: ' + avarage + '. Your grade:' + grade + '. You failed the course.';
-  }
-  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
